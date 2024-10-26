64aa10504fd936d269b349deff7414cd3703769a
ERuvinga <ruvingaelie@gmail.com>
2024-10-26 05:40:01 +0200

    Adding States Manager Loader in App

--- src/state/user/index.ts
+++ src/state/user/index.ts
@@ -1,3 +1,9 @@
+import {
+  AppointmentDatas,
+  CommentDatas,
+  FILTERSPOSTS,
+  PostWithCommentsDatas,
+} from '@/Constants/Type';
 import { atom } from 'recoil';
 
 export const dataUser = atom({
@@ -8,4 +14,59 @@ export const dataUser = atom({
 export const PlatformInfo = atom({
   key: 'PlatformInfo',
   default: '',
+});
+
+//Filter datas
+export const HomeFilterSelected = atom({
+  key: 'HomeFilterSelected',
+  default: FILTERSPOSTS.POSTS,
+});
+
+export const ProfileFilterSelected = atom({
+  key: 'ProfileFilterSelected',
+  default: 0,
+  // useLocalStorage().getAccountType() == UserRole.CITIZEN
+  //     ? FILTERSPOSTS.DEBATES
+  //     : FILTERSPOSTS.POSTS,
+});
+
+// Select Datas
+export const PostSelected = atom({
+  key: 'PostSelected',
+  default: {} as PostWithCommentsDatas,
+});
+
+export const PostWithoutLikesSelected = atom({
+  key: 'PostWithoutLikesSelected',
+  default: {} as PostWithCommentsDatas,
+});
+
+export const CommentSelected = atom({
+  key: 'CommentSelected',
+  default: {} as CommentDatas,
+});
+
+//Meeting States
+export const MeetingSelected = atom({
+  key: 'MeetingSelected',
+  default: {} as AppointmentDatas,
+});
+
+// Loader Home Pages datas
+export const ForumsLoader = atom({
+  key: 'ForumsLoader',
+  default: true,
+});
+
+export const DebatesLoader = atom({
+  key: 'DebatesLoader',
+  default: true,
+});
+export const PollsLoader = atom({
+  key: 'PollsLoader',
+  default: true,
+});
+export const MeetingLoader = atom({
+  key: 'MeetingLoader',
+  default: true,
 });
