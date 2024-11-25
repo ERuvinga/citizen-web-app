adaf6510012847d8ddd94db19ceeaa49ed3c6839
ERuvinga <ruvingaelie@gmail.com>
2024-11-25 07:59:42 +0200

    Finish Authentification Flow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  images: {
-    domains: ['s.france24.com', 'www.google.com'],
-  },
 };
 
 module.exports = nextConfig;
